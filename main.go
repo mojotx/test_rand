8be52e53f96f3eb911d1fb99917877befd74e953
Michael Jarvis <michael@jarvis.com>
2023-11-22 06:46:51 -0600

    Refactored and moved result into separate package

--- main.go
+++ main.go
@@ -1,66 +1,15 @@
 package main
 
 import (
-	"crypto/rand"
 	"fmt"
-	"math"
 	"math/big"
+	"os"
 
+	resultPkg "github.com/mojotx/test_rand/pkg/result"
 	"github.com/montanaflynn/stats"
 )
 
 var limit = big.NewInt(100)
-
-type Result struct {
-	Champion         uint64
-	ChampionPercent  float64
-	Challenge        uint64
-	ChallengePercent float64
-	Min              uint64
-	Max              uint64
-}
-
-func doWork() Result {
-	var n uint64
-
-	var min, max uint64
-	min = math.MaxUint64
-
-	var challenge, champion uint64
-
-	for ; n < 1000; n++ {
-		ri, _ := rand.Int(rand.Reader, limit)
-
-		n := ri.Uint64()
-		// fmt.Print(n)
-
-		if n < min {
-			min = n
-		}
-
-		if n > max {
-			max = n
-		}
-
-		if n < 30 {
-			// fmt.Println(" challenge")
-			challenge++
-		} else {
-			// fmt.Println(" champion")
-			champion++
-		}
-	}
-
-	sum := challenge + champion
-	return Result{
-		Champion:         champion,
-		ChampionPercent:  float64(champion) / float64(sum) * 100.0,
-		Challenge:        challenge,
-		ChallengePercent: float64(challenge) / float64(sum) * 100.0,
-		Min:              min,
-		Max:              max,
-	}
-}
 
 func main() {
 
@@ -68,12 +17,9 @@ func main() {
 
 	for n := 0; n < 1000; n++ {
 
-		result := doWork()
+		result := resultPkg.DoWork()
 		values = append(values, result.ChallengePercent)
-		if result.ChallengePercent <= 20.0 {
-			fmt.Printf("%d: %d (%f%%) - %d (%f%%) - %d - %d\n", n, result.Champion, result.ChampionPercent, result.Challenge, result.ChallengePercent, result.Min, result.Max)
-		}
-
+		resultPkg.CheckResult(os.Stderr, n, result)
 	}
 
 	// mean or average
---
+++ pkg/result/result.go
@@ -0,0 +1,72 @@
+package result
+
+import (
+	"crypto/rand"
+	"fmt"
+	"io"
+	"math"
+	"math/big"
+)
+
+type Result struct {
+	Champion         uint64
+	ChampionPercent  float64
+	Challenge        uint64
+	ChallengePercent float64
+	Min              uint64
+	Max              uint64
+}
+
+var percentage *big.Int
+
+func init() {
+	percentage = big.NewInt(100)
+}
+
+func DoWork() Result {
+	var n uint64
+
+	var min, max uint64
+	min = math.MaxUint64
+
+	var challenge, champion uint64
+
+	for ; n < 1000; n++ {
+		ri, _ := rand.Int(rand.Reader, percentage)
+
+		n := ri.Uint64()
+
+		if n < min {
+			min = n
+		}
+
+		if n > max {
+			max = n
+		}
+
+		if n < 30 {
+			challenge++
+		} else {
+			champion++
+		}
+	}
+	sum := challenge + champion
+	return Result{
+		Champion:         champion,
+		ChampionPercent:  float64(champion) / float64(sum) * 100.0,
+		Challenge:        challenge,
+		ChallengePercent: float64(challenge) / float64(sum) * 100.0,
+		Min:              min,
+		Max:              max,
+	}
+}
+
+func CheckResult(w io.Writer, n int, result Result) {
+
+	if result.ChallengePercent <= 20.0 {
+		fmt.Fprintf(w, "%d: %d (%f%%) - %d (%f%%) - %d - %d\n", n,
+			result.Champion, result.ChampionPercent,
+			result.Challenge, result.ChallengePercent,
+			result.Min, result.Max)
+	}
+}
